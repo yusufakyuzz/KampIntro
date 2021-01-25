23558585d65b66f71a1d4c4ed2f1b8d22a3182f8
Asus-KA56CA <Asus-KA56CA@DESKTOP-1P1396C>
2021-01-25 14:25:36 +0300

    Add project files.

---
+++ GenericsDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDictionary
+{
+    class MyDictionary<K,V>
+    {
+        K[] anahtar;
+        V[] deger;
+        K[] geciciAnahtar;
+        V[] geciciDeger;
+
+        public MyDictionary()
+        {
+            anahtar = new K[0];
+            deger = new V[0];
+
+        }
+
+        public void Add(K item, V value)
+        {
+            geciciAnahtar = anahtar;
+            geciciDeger = deger;
+            anahtar = new K[anahtar.Length + 1];
+            deger = new V[deger.Length + 1];
+
+            for(int i=0;i<geciciAnahtar.Length;i++)
+            {
+                anahtar[i] = geciciAnahtar[i];
+                deger[i] = geciciDeger[i];
+            }
+
+            anahtar[anahtar.Length - 1] = item;
+            deger[deger.Length - 1] = value;
+
+        }
+
+        public int Length()
+        {
+
+            return anahtar.Length;
+        }
+    
+    }
+}
---
+++ GenericsDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>();
+            ogrenci.Add(1, "Yusuf");
+            ogrenci.Add(1, "Akyüz");
+            ogrenci.Add(1, "Engin Demiroğ");
+            ogrenci.Add(1, "Yıldız ver");
+            ogrenci.Add(1, "Ankara");
+            ogrenci.Add(1, "Yozgat");
+            ogrenci.Add(1, "Yusuf");
+            Console.WriteLine(""+ogrenci.Length());
+
+        }
+    }
+}
