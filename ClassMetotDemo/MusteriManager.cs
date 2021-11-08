45e68e0406880c5434b83d386a55e62cc69fc74d
Yusuf Akyüz <Yusufakyuzz@hotmail.com>
2021-11-08 23:18:07 +0300

    Ödev3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad+" : müşterisi eklendi.");
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " : müşteri bilgileri getirildi.");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " : müşterisi silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Yusuf";
+            musteri1.Soyad = "Akyüz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Birgül";
+            musteri2.Soyad = "Akyüz";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Yavuz";
+            musteri3.Soyad = "Yücel";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            Console.WriteLine("----------FOREACH BAŞLADI------------");
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine(musteri.Id+" IDsine sahip "+ musteri.Ad+" "+musteri.Soyad+" isimli müşteri bulundu.");
+                
+            }
+            Console.WriteLine("----------FOREACH SONU------------");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Listele(musteri3);
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
